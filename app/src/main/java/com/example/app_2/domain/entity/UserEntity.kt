5d50532911b4dee99665218faeecff54065ed03c
ASUS <antonyzapana550@gmail.com>
2025-07-07 12:35:01 -0500

    USERENTITY CONT.

--- app/src/main/java/com/example/app_2/domain/entity/UserEntity.kt
+++ app/src/main/java/com/example/app_2/domain/entity/UserEntity.kt
@@ -1,2 +1,14 @@
 package com.example.app_2.domain.entity
 
+data class UserEntity(
+    val userId: String,
+    val userName: String,
+    val userEmail: String,
+    val userType: UserMode
+)
+sealed class UserMode(val userType: Int) {
+    data object STUDENT_USER : UserMode(userType = 0)
+    data object TEACHER_USER : UserMode(userType = 1)
+    data object COORDINADOR_USER : UserMode(userType = 2)
+    data object ADMIN_USER : UserMode(userType = 3)
+}
